51a625e916421886f8288a013cc4101a0d675f13
roodev <randyodoom19@gmail.com>
2022-04-16 14:15:54 +0000

    firebase

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+   reactStrictMode: true,
+   env: {
+      SECRETE_API:
+         "sk_test_51JzW17Hj7SFJ7TixYDUlLuYabfaLGhoAYVR3aUsmqxMD6KFDWx6aYi4F1W95I0weWsOrOkrkYvUPqeolSqvj5ZLl00xkOQmp0n",
+   },
+};
