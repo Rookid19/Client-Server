015b39e5e5cfe009b9d983153436d84f968077bb
roodev <randyodoom19@gmail.com>
2022-04-16 14:05:05 +0000

    firebase

--- pages/api/client.js
+++ pages/api/client.js
@@ -9,8 +9,10 @@ export default async function handler(req, res) {
    querySnapshot.forEach((doc) => {
       mainData.push(doc.data().email);
    });
+   let IEX_API_KEY = "pk_90c3c41220f34be0923e7849242f4814";
+   let qouteUrl = `https://cloud.iexapis.com/stable/stock/AAPL/quote?token=${IEX_API_KEY}`;
 
-   console.log(mainData[1])
+   console.log("qoute----> " + qouteUrl);
    //    setInterval(() => {
    //       for (let i = 0; i < mainData.length; i++) {
    //          fetch("http://localhost:3001/api/server", {
